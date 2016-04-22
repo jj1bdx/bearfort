50f401f551b3717a536d3a154ab18110571b96e1
Kenji Rikitake <kenji.rikitake@acm.org>
2016-04-22 14:28:34 +0900

    bearfort-arduino.c: change copyright years

--- arduino-uno/bearfort-arduino.c
+++ arduino-uno/bearfort-arduino.c
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2009-2015 Kenji Rikitake
+ * Copyright (c) 2015-2016 Kenji Rikitake
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
